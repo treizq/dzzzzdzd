431e4a383f7dac303b3952558c0a6c6b5566d5d8
treizq <krokusforksovich@gmail.com>
2025-04-12 14:30:08 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp8/PrimeNumbers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class PrimeNumbers
+    {
+        public IEnumerable<int> GetPrimes(int limit)
+        {
+            for (int i = 2; i <= limit; i++)
+            {
+                if (IsPrime(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        public IEnumerable<int> GetAllPrimes()
+        {
+            int number = 2;
+            while (true)
+            {
+                if (IsPrime(number))
+                {
+                    yield return number; 
+                }
+                number++;
+            }
+        }
+
+        public IEnumerable<int> SkipPrimes(int count)
+        {
+            int skipped = 0;
+            foreach (var prime in GetAllPrimes())
+            {
+                if (skipped >= count)
+                {
+                    yield return prime;
+                }
+                skipped++;
+            }
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp8;
+
+public class Program
+{
+    public static void Main()
+    {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+
+        //GetPrimes
+        Console.WriteLine("Простые числа до 20:");
+        foreach (var prime in primeNumbers.GetPrimes(20))
+        {
+            Console.Write(prime + " ");
+        }
+        Console.WriteLine();
+
+        //GetAllPrimes (первые 10 простых чисел)
+        Console.WriteLine("Первые 10 простых чисел:");
+        int count = 0;
+        foreach (var prime in primeNumbers.GetAllPrimes())
+        {
+            Console.Write(prime + " ");
+            count++;
+            if (count == 10)
+            {
+                break;
+            }
+        }
+        Console.WriteLine();
+
+        //SkipPrimes (пропустить первые 5 простых чисел)
+        Console.WriteLine("Простые числа после пропуска первых 5:");
+        foreach (var prime in primeNumbers.SkipPrimes(5))
+        {
+            Console.Write(prime + " ");
+            if (prime > 20) 
+            {
+                break;
+            }
+        }
+        Console.WriteLine();
+    }
+}
